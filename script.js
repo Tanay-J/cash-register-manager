3f502ef034ee15b19a2e8bf38d9103ee90c8ac3b
Tanay <tanay.joshi9@gmail.com>
2021-07-21 14:52:58 +0530

    display change amount and denominations

--- script.js
+++ script.js
@@ -28,24 +28,96 @@ const displayController = (() => {
         }
     })
 
-    return {billAmount,paidAmount}
+    const notesDisplay = (notesArray) => {
+        const notesContainer = document.createElement('div');
+        notesContainer.classList.add('notes-container');
+        // notesContainer.textContent = 'Amount to be returned: '+ `${notesArray[9]}`;
+        container.appendChild(notesContainer);
+
+        const h1 = document.createElement('h2');
+        h1.classList.add('remaining-amount');
+        h1.textContent = 'Amount to be returned: '+ `${notesArray[9]}`;
+        notesContainer.appendChild(h1);
+
+        const h2 = document.createElement('h3');
+        const h3 = document.createElement('h3');
+        h2.classList.add('table-heading');
+        h3.classList.add('table-heading');
+        h2.textContent = 'Denomination';
+        h3.textContent = 'No. of notes';
+        notesContainer.appendChild(h2);
+        notesContainer.appendChild(h3);
+
+        for(let i = 0; i < notesArray.length-1; i++){
+            
+            const p2 = document.createElement('p');
+            const p3 = document.createElement('p');
+           
+            p2.classList.add('note');
+            p3.classList.add('no-of-notes');
+
+
+            if(i == 0){
+                p2.textContent = '1000';
+                p3.textContent = `${notesArray[i]}`;
+                // p2.setAttribute('style','backgroundColor')
+                notesContainer.appendChild(p2);
+                notesContainer.appendChild(p3);
+            }else if(i == 1){
+                p2.textContent = '500';
+                p3.textContent = `${notesArray[i]}`;
+                notesContainer.appendChild(p2);
+                notesContainer.appendChild(p3);
+            }else if(i == 2){
+                p2.textContent = '100';
+                p3.textContent = `${notesArray[i]}`;
+                notesContainer.appendChild(p2);
+                notesContainer.appendChild(p3);
+            }else if(i == 3){
+                p2.textContent = '50';
+                p3.textContent = `${notesArray[i]}`;
+                notesContainer.appendChild(p2);
+                notesContainer.appendChild(p3);
+            }else if(i == 4){
+                p2.textContent = '20';
+                p3.textContent = `${notesArray[i]}`;
+                notesContainer.appendChild(p2);
+                notesContainer.appendChild(p3);
+            }else if(i == 5){
+                p2.textContent = '10';
+                p3.textContent = `${notesArray[i]}`;
+                notesContainer.appendChild(p2);
+                notesContainer.appendChild(p3);
+            }else if(i == 6){
+                p2.textContent = '5';
+                p3.textContent = `${notesArray[i]}`;
+                notesContainer.appendChild(p2);
+                notesContainer.appendChild(p3);
+            }else if(i == 7){
+                p2.textContent = '2';
+                p3.textContent = `${notesArray[i]}`;
+                notesContainer.appendChild(p2);
+                notesContainer.appendChild(p3);
+            }else if(i == 8){
+                p2.textContent = '1';
+                p3.textContent = `${notesArray[i]}`;
+                notesContainer.appendChild(p2);
+                notesContainer.appendChild(p3);
+            }
+
+            
+           
+
+        }
+    }
+    return {billAmount,paidAmount,notesDisplay}
 })();
 
 const changeCalculator = () => {
     let difference = displayController.paidAmount.value - displayController.billAmount.value;
-    // let thousandNotes = 0;
-    // let fivehundredNotes = 0;
-    // let hundredNotes = 0;
-    // let fiftyNotes = 0;
-    // let twentyNotes = 0;
-    // let tenNotes = 0;
-    // let fiveCoins = 0
-    // let twoCoins = 0;
-    // let oneCoins = 0;
+    let notesArray = [0,0,0,0,0,0,0,0,0,difference];
 
-    let notesArray = [0,0,0,0,0,0,0,0,0];
-
-    for(let i = 0; i < notesArray.length; i++){
+    for(let i = 0; i < notesArray.length-1; i++){
         if(difference > 1000){
             notesArray[0] = parseInt(difference/1000);
             difference -= notesArray[0]*1000;
@@ -70,11 +142,12 @@ const changeCalculator = () => {
         }else if(difference > 2){
             notesArray[7] = parseInt(difference/2);
             difference -= notesArray[7]*2;       
-        }else if(difference = 1){
+        }else if(difference == 1){
             notesArray[8] = parseInt(difference);
             difference -= notesArray[8];     
         }
     }
+    displayController.notesDisplay(notesArray);
    
 
     return notesArray;
